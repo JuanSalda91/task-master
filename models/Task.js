a7bcfc373a32a239768633ed45435b61fe922452
JuanSalda91 <juansalda9131@gmail.com>
2026-01-28 09:27:32 -0500

    task schema/model created. linked to project schema

--- models/Task.js
+++ models/Task.js
@@ -3,3 +3,25 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // 2. define Task schema/model
+const taskSchema = new Schema (
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        status: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project', // model name from Project.js
+            required: true, // every task must belong to a project
+        },
+    },
+    {
+        timeStamps: true,
+    }
+);
+
+// 3. create and export task model
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
