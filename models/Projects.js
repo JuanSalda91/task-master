64895b2deb92235208d9bacb08fe76078a913607
JuanSalda91 <juansalda9131@gmail.com>
2026-02-01 01:39:57 -0500

    file name fixed

---
+++ models/Projects.js
@@ -0,0 +1,33 @@
+// 1. imoprt mongoosee to defime schema/model
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// 2. define project schema
+const projectSchema = new Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+        user: {
+            // here's were project links to the user who owns it
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true, // createdAt / updatedAt
+    }
+);
+
+// 3. create and export project model
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
